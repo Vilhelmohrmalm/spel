027aa3484d8e9b84d22ac02306f41090fd558d69
Vilhelmohrmalm <115473351+Vilhelmohrmalm@users.noreply.github.com>
2022-11-11 12:41:20 +0100

    kalibrerade styrka

--- Karaktärer.py
+++ Karaktärer.py
@@ -1,18 +1,16 @@
 class karaktär():
-    def _init_(self, p_hp, spe, p_str):
+    def _init_(self, p_hp, p_str):
         self.p_hp=p_hp
-        self.spe=spe
         self.p_str=p_str
 
-assasin= karaktär(3, 10, 7)
-barb= karaktär(8,2,10)
-knight= karaktär(10,6,4)
+assasin= karaktär(5, 10)
+barb= karaktär(8, 7)
+knight= karaktär(11, 4)
 
 class monster():
     def _init_(self, m_hp, m_str):
         self.m_hp=m_hp
         self.m_str=m_str
 
-slime= monster(1-4, 1-2)
-goblin= monster(1-3, 2-4)
-print("hej")
+slime= monster(4-8, 2-7)
+goblin= monster(3-7, 4-9)
