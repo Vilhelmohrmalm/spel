9efe92c43807dfab77906902edcdecbc98eaccc2
vilhelmohrmalm <vilhelm.ohrmalm@skoal.taby.se>
2022-11-14 14:52:14 +0100

    asdf

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
-import random 
+
+import random
 
 def m_health(a,b):
     health=[]
@@ -12,11 +13,33 @@ def m_damage(a,b):
         damage.append(i)
     return(random.choice(damage))
 
+class karaktär():
+    def __init__(self, p_hp, spe, p_str):
+        self.p_hp=p_hp
+        self.spe=spe
+        self.p_str=p_str
+
+# h
+
+class monster():
+    def __init__(self, m_hp, m_str, namn):
+        self.namn = namn
+        self.m_hp=m_hp
+        self.m_str=m_str
+
+    def __str__(self):
+        return f"Det här är en {self.namn}, den har {self.m_hp} hp och {self.m_str} styrka"
+
+
+slime= monster(m_health(1,4), m_damage(1,2), "slime")
+goblin= monster(m_health(1,3), m_damage(2,4), "goblin")
+
+
+
 
         
-        
 def main():
-    print(m_health(4,11))
-    print(m_damage(1,6))
+    print(slime)
+    print(goblin)
 
 main()
