70d36f3daec9a81f6d0d12963a70fe7281777c2d
vilhelmohrmalm <vilhelm.ohrmalm@skoal.taby.se>
2022-11-14 15:18:30 +0100

    Funktion för monster hp och str

--- Karaktärer.py
+++ Karaktärer.py
@@ -14,23 +14,28 @@ def m_damage(a,b):
     return(random.choice(damage))
 
 class karaktär():
-    def __init__(self, p_hp, spe, p_str):
+    def __init__(self, p_hp, p_str, p_namn):
         self.p_hp=p_hp
-        self.spe=spe
         self.p_str=p_str
+        self.p_namn=p_namn
 
-# h
+
+
 
 class monster():
-    def __init__(self, m_hp, m_str, namn):
-        self.namn = namn
+    def __init__(self, m_hp, m_str, m_namn):
+        self.namn =m_namn
         self.m_hp=m_hp
         self.m_str=m_str
 
     def __str__(self):
-        return f"Det här är en {self.namn}, den har {self.m_hp} hp och {self.m_str} styrka"
+        return f"Du stöter på en {self.m_namn}, den har {self.m_hp} hp och {self.m_str} styrka"
 
 
-slime= monster(m_health(1,4), m_damage(1,2), "slime")
-goblin= monster(m_health(1,3), m_damage(2,4), "goblin")
+slime= monster(m_health(4,11), m_damage(1,6), "slime")
+goblin= monster(m_health(3,7), m_damage(3,8), "goblin")
+
+assasin= karaktär(5, 10, "assasin")
+barb= karaktär(8, 7, "barb")
+knight= karaktär(10, 5, "knight")
 
--- test.py
+++ test.py
@@ -14,24 +14,24 @@ def m_damage(a,b):
     return(random.choice(damage))
 
 class karaktär():
-    def __init__(self, p_hp, spe, p_str):
+    def __init__(self, p_hp, p_str,p_namn):
         self.p_hp=p_hp
-        self.spe=spe
         self.p_str=p_str
+        self.p_namn=p_namn
 
 # h
 
 class monster():
-    def __init__(self, m_hp, m_str, namn):
-        self.namn = namn
+    def __init__(self, m_hp, m_str,m_namn):
+        self.namn =m_namn
         self.m_hp=m_hp
         self.m_str=m_str
 
     def __str__(self):
-        return f"Det här är en {self.namn}, den har {self.m_hp} hp och {self.m_str} styrka"
+        return f"Det här är en {self.m_namn}, den har {self.m_hp} hp och {self.m_str} styrka"
 
 
-slime= monster(m_health(1,4), m_damage(1,2), "slime")
+slime= monster(m_health(4,11), m_damage(1,6), "slime")
 goblin= monster(m_health(1,3), m_damage(2,4), "goblin")
 
 
