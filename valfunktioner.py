421c8cc017ac07ae6b5c742c85bdc512b8a039d8
vilhelmohrmalm <vilhelm.ohrmalm@skoal.taby.se>
2022-11-21 14:47:04 +0100

    Ändrade i Valfunktionerna

--- valfunktioner.py
+++ valfunktioner.py
@@ -1,7 +1,7 @@
 
-def val():
+def val_vanlig():
     val = input(
-        "vad vill du göra?\n K = kamp\n S = stats\n Ö = öppna\n V = vänster\n H = höger\n ")
+        "vad vill du göra?\n S = stats\n  V = vänster\n H = höger\n ")
     if val == "S" or val == "stats" or val == "s":
         print("din stats är dina stats")
     elif val == "V" or val == "vänster" or val == "v":
@@ -10,7 +10,24 @@ def val():
         print("du gick igen om dörren till höger och kommer till...")
     elif val == "Ö" or val == "öppna" or val == "ö":
         print("du öppnar kistan och med ett gnistlande ljud så ser du hur en stor  i den...")
-    elif val == "K" or val == "kamp" or val == "k":
-        print("du knyter dina nävar och gör dig redo...")
     else:
         print("din sopa välj ett av alternativen")
+        val_vanlig()
+
+        
+def val_kista():
+    val = input(
+        "vad vill du göra?\n S = stats\n Ö = öppna\n V = vänster\n H = höger\n ")
+    if val == "S" or val == "stats" or val == "s":
+        print("din stats är dina stats")
+        val_kista()
+    elif val == "V" or val == "vänster" or val == "v":
+        print("du gick igen om dörren till vänster och kommer till...")
+    elif val == "H" or val == "höger" or val == "h":
+        print("du gick igen om dörren till höger och kommer till...")
+    elif val == "Ö" or val == "öppna" or val == "ö":
+        print("du öppnar kistan och med ett gnistlande ljud så ser du hur en stor  i den...")
+        val_vanlig()
+    else:
+        print("din sopa välj ett av alternativen")
+        val_kista()
