681ada64cc25dec191a49243741132db55f7a931
Hilmer <hilmer.burgstaller@skola.taby.se>
2022-11-30 22:47:50 +0100

    skapade vapen använde health o damag från tidigare

--- Karaktärer.py
+++ Karaktärer.py
@@ -1,46 +1,44 @@
 from text_animering import *
 import random
 
-def m_health(a,b):
-    health=[]
-    for i in range(a,b+1):
-        health.append(i)
-    return(random.choice(health))
 
-def m_damage(a,b):
-    damage=[]
-    for i in range(a,b+1):
+def health(a, b):
+    health = []
+    for i in range(a, b+1):
+        health.append(i)
+    return (random.choice(health))
+
+
+def damage(a, b):
+    damage = []
+    for i in range(a, b+1):
         damage.append(i)
-    return(random.choice(damage))
+    return (random.choice(damage))
+
 
 class Karaktär():
     def __init__(self, p_hp, p_str, p_namn):
-        self.p_hp=p_hp
-        self.p_str=p_str
-        self.p_namn=p_namn
+        self.p_hp = p_hp
+        self.p_str = p_str
+        self.p_namn = p_namn
 
     def print_stats(self):
         typingPrint(f"Din hp är {self.p_hp} och din str är {self.p_str}")
-        
-
-
 
 
 class Monster():
     def __init__(self, m_hp, m_str, m_namn):
-        self.m_namn =m_namn
-        self.m_hp=m_hp
-        self.m_str=m_str
+        self.m_namn = m_namn
+        self.m_hp = m_hp
+        self.m_str = m_str
 
     def __str__(self):
         return f"Du stöter på en {self.m_namn}, den har {self.m_hp} hp och {self.m_str} styrka"
 
 
-Slime= Monster(m_health(4,11), m_damage(1,6), "Slime")
-Goblin= Monster(m_health(3,7), m_damage(3,8), "Goblin")
+Slime = Monster(health(4, 11), damage(1, 6), "Slime")
+Goblin = Monster(health(3, 7), damage(3, 8), "Goblin")
 
-Assasin= Karaktär(5, 10, "assasin")
-Barb= Karaktär(8, 7, "Barb")
-Knight= Karaktär(10, 5, "Knight")
-
-
+Assasin = Karaktär(5, 10, "assasin")
+Barb = Karaktär(8, 7, "Barb")
+Knight = Karaktär(10, 5, "Knight")
---
+++ vapen.py
@@ -0,0 +1,12 @@
+from Karaktärer import *
+
+
+class Vapen():
+    def __init__(self, v_hp, v_str, v_namn):
+        self.v_namn = v_namn
+        self.v_hp = v_hp
+        self.v_str = v_str
+
+
+Svärd = Vapen(health(0, 1), damage(1, 4), "Svärd")
+Sköld = Vapen(health(2, 3), damage(0, 1), "Sköld")
