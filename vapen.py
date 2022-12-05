6e45f5cef3afab7ba6a6406417f76da66b46568c
Hilmer <hilmer.burgstaller@skola.taby.se>
2022-12-05 14:54:43 +0100

    la till fler vapen

--- vapen.py
+++ vapen.py
@@ -8,5 +8,8 @@ class Vapen():
         self.v_str = v_str
 
 
-Svärd = Vapen(health(0, 1), damage(1, 4), "Svärd")
-Sköld = Vapen(health(2, 3), damage(0, 1), "Sköld")
+Svärd = Vapen(health(0, 1), damage(1, 5), "Svärd")
+Sköld = Vapen(health(4, 8), damage(0, 0), "Sköld")
+Yxa = Vapen(health(0, 0), damage(2, 6), "Yxa")
+Pilbåge = Vapen(health(0, 0), damage(4, 5), "Pilbåge")
+Spjut = Vapen(health(0, 0), damage(1, 7), "Spjut")
