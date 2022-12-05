174a013d00a3d9b4cc15d0cd2c4ac77bef6399c2
Hilmer <hilmer.burgstaller@skola.taby.se>
2022-12-05 14:57:56 +0100

    ändrade vapen-stats

--- vapen.py
+++ vapen.py
@@ -8,8 +8,8 @@ class Vapen():
         self.v_str = v_str
 
 
-Svärd = Vapen(health(0, 1), damage(1, 5), "Svärd")
-Sköld = Vapen(health(4, 8), damage(0, 0), "Sköld")
-Yxa = Vapen(health(0, 0), damage(2, 6), "Yxa")
-Pilbåge = Vapen(health(0, 0), damage(4, 5), "Pilbåge")
-Spjut = Vapen(health(0, 0), damage(1, 7), "Spjut")
+Svärd = Vapen(health(0, 1), damage(1, 2), "Svärd")
+Sköld = Vapen(health(2, 3), damage(0, 0), "Sköld")
+Yxa = Vapen(health(0, 0), damage(1, 4), "Yxa")
+Pilbåge = Vapen(health(0, 0), damage(2, 3), "Pilbåge")
+Spjut = Vapen(health(0, 0), damage(1, 4), "Spjut")
