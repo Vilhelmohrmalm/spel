3d4a0a6eb126af0e0173ca6d6ccedfa72ccaa3d0
vilhelmohrmalm <vilhelm.ohrmalm@skoal.taby.se>
2022-12-16 12:39:50 +0100

    hje

--- rumtyp+.py
+++ rumtyp+.py
@@ -20,13 +20,13 @@ def kista(Spelar_stats):
 kista()
 
 
-def rum_typ(m_hp, m_str, p_hp, p_str):
+def rum_typ( p_hp, p_str):
     typ = random.randint(1, 10)
     if typ == 1 or typ == 2 or typ == 3 or typ == 4:
-        fight(m_hp, m_str, p_hp, p_str)
-        val_vanlig(m_hp, m_str, p_hp, p_str)
+        fight( p_hp, p_str)
+        val_vanlig( p_hp, p_str)
     elif typ == 5 or typ == 6 or typ == 7 or typ == 8:
-        val_vanlig(m_hp, m_str, p_hp, p_str)
+        val_vanlig( p_hp, p_str)
     elif typ == 9:
         val_kista()
     elif typ == 10:
