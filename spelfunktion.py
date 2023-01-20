75e09fd83fa16b333a7cafd43efa8bb5a12fcc28
vilhelmohrmalm <vilhelm.ohrmalm@skoal.taby.se>
2023-01-20 13:33:52 +0100

    hej

--- spelfunktion.py
+++ spelfunktion.py
@@ -114,27 +114,27 @@ def fälla(spelar_stats):
 # ---------------------------------------- VALFUNKTIONER ----------------------------------------
 
 
-def val_vanlig(spelar_stats):
+def val_vanlig(spelar_stats,v_hp,v_str,v_namn):
     val = typingInput(
         "vad vill du göra?\n S = stats\n V = vänster\n F = fram\n H = höger\n R = ryggsäck\n")
     if val in ["S", "stats", "s"]:
         typingPrint(
             f"Du har hp {spelar_stats.p_hp} och din str är {spelar_stats.p_str}\n")
-        val_vanlig(spelar_stats)
+        val_vanlig(spelar_stats,v_hp,v_str,v_namn)
     elif val in ["V", "vänster", "v"]:
         typingPrint("du gick igen om dörren till vänster ")
-        rum_typ(spelar_stats)
+        rum_typ(spelar_stats,v_hp,v_str,v_namn)
     elif val in ["H", "höger", "h"]:
         typingPrint("du gick igen om dörren till höger och kommer till ")
-        rum_typ(spelar_stats)
+        rum_typ(spelar_stats,v_hp,v_str,v_namn)
     elif val in ["R", "ryggsäck", "r"]:
         typingPrint("hej")
     elif val in ["F", "fram", "f"]:
         typingPrint("Du går fram och kommer till ")
-        rum_typ(spelar_stats)
+        rum_typ(spelar_stats,v_hp,v_str,v_namn)
     else:
         typingPrint("din sopa välj ett av alternativen\n")
-        val_vanlig(spelar_stats)
+        val_vanlig(spelar_stats,v_hp,v_str,v_namn)
 
 
 def val_kista(spelar_stats, v_hp, v_str, v_namn):
