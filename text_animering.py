9dcbdeb4aa9c443533c6e597108adb0539ff26d1
Hilmer <hilmer.burgstaller@skola.taby.se>
2023-01-23 21:01:51 +0100

    kuk6

--- text_animering.py
+++ text_animering.py
@@ -8,14 +8,14 @@ def typingPrint(text):
     for character in text:
         sys.stdout.write(character)
         sys.stdout.flush()
-        time.sleep(0.04)
+        time.sleep(0.02)
 
 
 def typingInput(text):
     for character in text:
         sys.stdout.write(character)
         sys.stdout.flush()
-        time.sleep(0.04)
+        time.sleep(0.02)
     value = input()
     return value
 
